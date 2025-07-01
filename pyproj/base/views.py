8384db2106cd0c3e6946e5b9bf0e0020c3716e5f
DragonFist <abawaneh580@gmail.com>
2025-07-02 00:36:11 +0300

    update

--- pyproj/base/views.py
+++ pyproj/base/views.py
@@ -1,9 +1,8 @@
 
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to the home page!")
+    return render(request , "home.html")
 
 def room(request):
-    return HttpResponse("ROOM")
+    return render(request, "room.html")
