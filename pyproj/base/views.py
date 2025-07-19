6864e5f9cbf445ffc2910623f3679dfa6dc07968
DragonFist <abawaneh580@gmail.com>
2025-07-19 23:18:05 +0300

    update

--- pyproj/base/views.py
+++ pyproj/base/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render , redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room , Topic
 from .form import RoomForm
 
 # rooms = [
@@ -11,8 +12,19 @@ from .form import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {"rooms" : rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+            Q(topic__name__icontains = q) |
+            Q(name__contains = q) |
+            Q(descreption__contains = q)
+            )
+
+    topics = Topic.objects.all()
+    rooms_count = rooms.count()
+
+    context = {"rooms" : rooms , "topics" : topics , "rooms_count" : rooms_count}
+
     return render(request , "base/home.html" , context)
 
 def room(request , pk):
