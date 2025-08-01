865cb9270fcc2264260300e1bbf8a244fa56c8fe
DragonFist <abawaneh580@gmail.com>
2025-08-01 21:27:21 +0300

    update

--- pyproj/base/views.py
+++ pyproj/base/views.py
@@ -1,6 +1,9 @@
 
 from django.shortcuts import render , redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login, logout
 from .models import Room , Topic
 from .form import RoomForm
 
@@ -11,6 +14,31 @@ from .form import RoomForm
 # ]
 
 
+def loginPage(request):
+    if request.method == "POST":
+
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request, "User does not exist.")
+
+        user = authenticate(request , username = username , password = password)
+
+        if user is not None:
+            login(request , user)
+            return redirect('home')
+        else:
+            messages.error(request, "Username OR Password does not exist")
+
+    context = {}
+    return render(request , "base/login_register.html" , context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
@@ -21,6 +49,7 @@ def home(request):
             )
 
     topics = Topic.objects.all()
+    
     rooms_count = rooms.count()
 
     context = {"rooms" : rooms , "topics" : topics , "rooms_count" : rooms_count}
